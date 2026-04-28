583fe63904a296f09917c140cdce426074a0b569
mohamed.bougrioua <mohamed.bougrioua@beyn.io>
2026-04-28 16:59:28 +0100

    feat: add put method

--- src/main/java/com/spring_boot/learn/controllers/EmployeeController.java
+++ src/main/java/com/spring_boot/learn/controllers/EmployeeController.java
@@ -2,10 +2,7 @@ package com.spring_boot.learn.controllers;
 
 import com.spring_boot.learn.entities.Employee;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -14,28 +11,50 @@ import java.util.UUID;
 
 public class EmployeeController {
     ArrayList<Employee> employees = new ArrayList<Employee>();
+
     @GetMapping
-    public ArrayList<Employee> findAll(){
+    public ArrayList<Employee> findAll() {
         return employees;
     }
+
     @GetMapping("/{employeeId}")
-    public Optional<Employee> helloWorld(@PathVariable UUID employeeId){
+    public Optional<Employee> helloWorld(@PathVariable UUID employeeId) {
         Optional<Employee> employee = employees.stream().filter(emp -> emp.getId().equals(employeeId)).findFirst();
         return employee;
     }
+
     @PostMapping
-    public Employee CreateOne(@RequestBody Employee employee){
+    public Employee CreateOne(@RequestBody Employee employee) {
         employee.setId(UUID.randomUUID());
         employee.setDepartementID(UUID.randomUUID());
         employees.add(employee);
         return employee;
     }
+
+    @PutMapping("/{EmployeeId}")
+    public Employee updateOne(@PathVariable UUID EmployeeId, @RequestBody Employee employee) {
+        Optional<Employee> employeeToUpdate = employees.stream().filter(emp -> emp.getId().equals(EmployeeId))
+                .findFirst();
+        if (employeeToUpdate.isPresent()) {
+            employeeToUpdate.get().setFirst_name(employee.getFirst_name());
+            employeeToUpdate.get().setLast_name(employee.getLast_name());
+            employeeToUpdate.get().setEmail(employee.getEmail());
+            employeeToUpdate.get().setPhone_number(employee.getPhone_number());
+            employeeToUpdate.get().setHire_date(employee.getHire_date());
+            employeeToUpdate.get().setPosition(employee.getPosition());
+            return employeeToUpdate.get();
+        } else {
+            System.err.println("cannot find the Emp pls retry later");
+            return null;
+        }
+    }
+
     @DeleteMapping("/{EmployeeId}")
-    public void deleteOne(@PathVariable UUID EmployeeId){
+    public void deleteOne(@PathVariable UUID EmployeeId) {
         Optional<Employee> employee = employees.stream().filter(emp -> emp.getId().equals(EmployeeId)).findFirst();
-        if(employee.isPresent()) {
+        if (employee.isPresent()) {
             employees.remove(employee.get());
-        }else{
+        } else {
             System.err.println("cannot find the Emp pls retry later");
         }
     }
